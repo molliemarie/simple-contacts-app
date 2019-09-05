2fe27f5496afc5de86a34cc79e7b59fd28476750
molliemarie <mollie.marie@gmail.com>
2019-09-05 16:23:34 -0700

    added new files; pass data into compoennt with props

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ListContacts from "./ListContacts";
+
+const contacts = [
+  {
+    id: "karen",
+    name: "Karen Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: "http://localhost:5001/karen.jpg"
+  },
+  {
+    id: "richard",
+    name: "Richard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: "http://localhost:5001/richard.jpg"
+  },
+  {
+    id: "tyler",
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg"
+  }
+];
+
+// class ContactList extends React.Component {
+//   render() {
+//     const people = this.props.contacts;
+//
+//     return (
+//       <ol>
+//         {people.map(person => (
+//           <li key={person.name}>{person.name}</li>
+//         ))}
+//       </ol>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <ListContacts contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
